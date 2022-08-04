a5cd2d699258b72e2a25f830cb8691bf0cbf7dc0
sthursby16 <104663899+sthursby16@users.noreply.github.com>
2022-08-04 15:45:40 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,93 @@
+function submit1() {
+    const n1adj1 = document.getElementById('1adj1').value;
+    document.getElementById('t1adj1').innerHTML = n1adj1;
+    const n1verb1 = document.getElementById('1verb1').value;
+    document.getElementById('t1verb1').innerHTML = n1verb1;
+    const n1noun1 = document.getElementById('1noun1').value;
+    document.getElementById('t1noun1').innerHTML = n1noun1; 
+    const n1noun2 = document.getElementById('1noun2').value;
+    document.getElementById('t1noun2').innerHTML = n1noun2;
+    const n1verb2 = document.getElementById('1verb2').value;
+    document.getElementById('t1verb2').innerHTML = n1verb2;
+    const n1adj2 = document.getElementById('1adj2').value;
+    document.getElementById('t1adj2').innerHTML = n1adj2;
+    const n1noun3 = document.getElementById('1noun3').value;
+    document.getElementById('t1noun3').innerHTML = n1noun3;
+    const n1verb3 = document.getElementById('1verb3').value;
+    document.getElementById('t1verb3').innerHTML = n1verb3;
+    const n1noun4 = document.getElementById('1noun4').value;
+    document.getElementById('t1noun4').innerHTML = n1noun4;
+    const n1adv1 = document.getElementById('1adv1').value;
+    document.getElementById('t1adv1').innerHTML = n1adv1;
+    const n1verb4 = document.getElementById('1verb4').value;
+    document.getElementById('t1verb4').innerHTML = n1verb4;
+    const n1noun5 = document.getElementById('1noun5').value;
+    document.getElementById('t1noun5').innerHTML = n1noun5;
+
+    document.getElementById("madlib1").style.display = "block";
+    document.getElementById("wordentry1").style.display = "none";
+
+}
+
+
+function submit2() {
+    const n2noun1 = document.getElementById('2noun1').value;
+    document.getElementById('t2noun1').innerHTML = n2noun1;
+    const n2adv1 = document.getElementById('2adv1').value;
+    document.getElementById('t2adv1').innerHTML = n2adv1;
+    const n2verb1 = document.getElementById('2verb1').value;
+    document.getElementById('t2verb1').innerHTML = n2verb1; 
+    const n2noun2 = document.getElementById('2noun2').value;
+    document.getElementById('t2noun2').innerHTML = n2noun2;
+    const n2verb2 = document.getElementById('2verb2').value;
+    document.getElementById('t2verb2').innerHTML = n2verb2;
+    const n2noun3 = document.getElementById('2noun3').value;
+    document.getElementById('t2noun3').innerHTML = n2noun3;
+    const n2noun4 = document.getElementById('2noun4').value;
+    document.getElementById('t2noun4').innerHTML = n2noun4;
+    const n2verb3 = document.getElementById('2verb3').value;
+    document.getElementById('t2verb3').innerHTML = n2verb3;
+    const n2noun5 = document.getElementById('2noun5').value;
+    document.getElementById('t2noun5').innerHTML = n2noun5;
+    const n2noun6 = document.getElementById('2noun6').value;
+    document.getElementById('t2noun6').innerHTML = n2noun6;
+    const n2adj1 = document.getElementById('2adj1').value;
+    document.getElementById('t2adj1').innerHTML = n2adj1;
+    const n2adj2 = document.getElementById('2adj2').value;
+    document.getElementById('t2adj2').innerHTML = n2adj2;
+
+    document.getElementById("madlib2").style.display = "block";
+    document.getElementById("wordentry2").style.display = "none";
+}
+
+
+function submit3() {
+    const n3adj1 = document.getElementById('3adj1').value;
+    document.getElementById('t3adj1').innerHTML = n3adj1;
+    const n3noun1 = document.getElementById('3noun1').value;
+    document.getElementById('t3noun1').innerHTML = n3noun1;
+    const n3verb1 = document.getElementById('3verb1').value;
+    document.getElementById('t3verb1').innerHTML = n3verb1; 
+    const n3adj2 = document.getElementById('3adj2').value;
+    document.getElementById('t3adj2').innerHTML = n3adj2;
+    const n3noun2 = document.getElementById('3noun2').value;
+    document.getElementById('t3noun2').innerHTML = n3noun2;
+    const n3noun3 = document.getElementById('3noun3').value;
+    document.getElementById('t3noun3').innerHTML = n3noun3;
+    const n3adv1 = document.getElementById('3adv1').value;
+    document.getElementById('t3adv1').innerHTML = n3adv1;
+    const n3noun4 = document.getElementById('3noun4').value;
+    document.getElementById('t3noun4').innerHTML = n3noun4;
+    const n3verb2 = document.getElementById('3verb2').value;
+    document.getElementById('t3verb2').innerHTML = n3verb2;
+    const n3verb3 = document.getElementById('3verb3').value;
+    document.getElementById('t3verb3').innerHTML = n3verb3;
+    const n3noun5 = document.getElementById('3noun5').value;
+    document.getElementById('t3noun5').innerHTML = n3noun5;
+    const n3noun6 = document.getElementById('3noun6').value;
+    document.getElementById('t3noun6').innerHTML = n3noun6;
+
+
+    document.getElementById("madlib3").style.display = "block";
+    document.getElementById("wordentry3").style.display = "none";
+}
